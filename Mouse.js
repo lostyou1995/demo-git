364c3c19ce6bd7904811f6dbe50e3971604f817f
lostyou1995 <ngocphuoc1012@gmail.com>
2018-10-15 23:02:09 +0700

    last commit

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,12 @@
+var chalk = require("chalk");
 function Mouse(name) {
     this.name = name;
     this.dead = false;
 }
 Mouse.prototype.die = function() {
     this.dead = true;
+}
+Mouse.prototype.sayHi = function () {
+    console.log("Hello",chalk.red(this.name));
 }
 module.exports = Mouse;
---
+++ index.js
@@ -0,0 +1,3 @@
+var Mouse = require("./Mouse");
+var mouse = new Mouse("Jerry");
+mouse.sayHi();
